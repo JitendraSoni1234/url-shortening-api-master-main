a16b6695b813e7021f9f75e7a13bdccbc62c467d
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-04-08 18:24:32 +0530

    completed url shortener

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { urlShortner } from "./api/urlApi";
+import urlSlice from './api/urlApi';
+
+export const store = configureStore({
+  reducer: {
+    url: urlSlice,
+    [urlShortner.reducerPath]: urlShortner.reducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(urlShortner.middleware),
+});
+
+setupListeners(store.dispatch);
